8021adc9755f5ee2c153fa06428fdd85d0d59f67
GoldenerReiter <rcosero@outlook.com>
2022-05-31 15:44:23 -0400

    asd

--- js/footer.js
+++ js/footer.js
@@ -16,28 +16,5 @@ let quote = JSON.parse(quoteJSON);
 
 let quotequote = quote.body;
 
-// translation of the quote
-
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com',
-		'X-RapidAPI-Key': '020d4230a9msh3631d4455749e93p193201jsndf1509523b22'
-	},
-	body: `{"q":"${quotequote}","source":"en","target":"es"}`
-};
-
-fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
-	.then(res => res.json())
-    .then((res) => JSON.stringify(res))
-	.then((res) => localStorage.setItem("translQuote", res))
-	.catch (footerQuoteQS.innerHTML = "Quote not found.");
-
-let translQuoteJSON = localStorage.getItem("translQuote");
-let translQuote = JSON.parse(translQuoteJSON);
-
-console.log(translQuote);
-
-footerQuoteQS.innerHTML = translQuote.data.translations.translatedText;
+footerQuoteQS.innerHTML = quotequote;
 authorQuoteQS.innerHTML = quote.author;
